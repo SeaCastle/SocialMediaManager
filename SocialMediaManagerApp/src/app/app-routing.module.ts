d93607c645d451060a9b180d2f0b270c0337d108
Spencer Peterson <spencerthomasp@gmail.com>
2018-09-13 17:46:55 -0600

    Added a dashboard component which we will use to display the main form and configured the routing component to display the dashboard by default.

--- SocialMediaManagerApp/src/app/app-routing.module.ts
+++ SocialMediaManagerApp/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: '/dashboard', component: DashboardComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- SocialMediaManagerApp/src/app/app.module.ts
+++ SocialMediaManagerApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
